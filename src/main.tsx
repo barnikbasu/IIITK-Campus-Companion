5678815e3d3e65f446d82e05b3908916936e7910
Barnik Basu <barnikbasu@gmail.com>
2026-04-05 10:30:51 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
